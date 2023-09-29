e2cbed26e89fe0afbd6f2fede2ae1e9fbbb3094d
SebastianTuriak <sebastian.turiak@gmail.com>
2023-09-29 09:16:19 +0200

    kalkulačka

---
+++ cv1.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Ahoj, ja som virtuálny papusek Kubo, rad opakujem!");
+Console.WriteLine("Napíš niečo čo budem opakovať: ");
+
+string vstup;
+vstup = Console.ReadLine();
+
+string vystup;
+vystup = vstup + ",   " + vstup + "!";
+Console.WriteLine(vystup);
+Console.ReadKey();
---
+++ cv2/cv2/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Vítajte v kalkulačke!");
+Console.WriteLine("Zadajte prvé číslo: ");
+string s = Console.ReadLine();
+float a = float.Parse(s);
+
+Console.WriteLine("Zadajte druhé číslo: ");
+string d = Console.ReadLine();
+float b = float.Parse(d);
+
+float sucet, rozdiel, sucin, podiel;
+
+sucet = a + b;
+rozdiel = a - b;
+sucin = a * b;
+podiel = a / b;
+
+Console.WriteLine("Súčet: " + sucet);
+Console.WriteLine("Rozdiel: " + rozdiel);
+Console.WriteLine("Súčin: " + sucin);
+Console.WriteLine("Podiel: " + podiel);
+
+Console.ReadKey();
