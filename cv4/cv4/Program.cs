9e7bd1e1a11f58063eacf9f9193b5777327d83ca
SebastianTuriak <sebastian.turiak@gmail.com>
2023-10-20 08:42:52 +0200

    nasobilka

---
+++ cv4/cv4/Program.cs
@@ -0,0 +1,17 @@
+﻿int nasobilka = 1;
+Console.WriteLine("Maĺa násobilka pomocou cyklu");
+
+Console.WriteLine("Zadaj číslo, od ktorého chceš začať");
+int zaciatok = int.Parse(Console.ReadLine());
+for (int i = zaciatok; i <= 10; i++)
+{
+    for (int j = 1; j <= 10; j++)
+    {
+
+        nasobilka = zaciatok * j;
+        Console.Write("{0} ", nasobilka);
+    }
+    Console.WriteLine();
+    zaciatok++;
+}
+Console.ReadKey();
